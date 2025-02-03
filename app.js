ff0b1f2e0017cd40fc7107af5b814e0b8b57e984
JorgeEGG <jkgonzalezg@hotmail.com>
2025-02-02 20:45:48 -0500

    Crear un array para almacenar los nombres de amigos e implementar funcion para agregar amigos en el array.

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let amigos = [];
 
-function agregarAmigos(){
-    let amigo = prompt()
+let listaAmigos = [];
+
+function agregarAmigo() {
+    let amigo = document.getElementById('amigo').value;
+    if (amigo == "") {
+        alert("Por favor, ingrese el nombre de un amigo:");
+        let amigo = document.getElementById('amigo').value;
+    }else{
+        listaAmigos.push(amigo);
+        document.getElementById('amigo').value = "";
+    }
+    console.log(listaAmigos);
 }
